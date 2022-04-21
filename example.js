3ba7e55a5b8b24f3ce5d20811db124d9d1cbf0f0
tuanvu19815 <101812790+tuanvu19815@users.noreply.github.com>
2022-04-22 00:57:16 +0700

    Add files via upload

---
+++ example.js
@@ -0,0 +1,41 @@
+var list = document.getElementsByTagName('ul')[0];
+
+var newItemlast = document.createElement('li');
+var newTextlast = document.createTextNode('cream');
+newItemlast.appendChild('newTextLast');
+list.appendChild(newItemlast);
+
+var newItemFirst = document.createElement('li');
+var newItemFirst = document.createTextNode('kale');
+newItemFirst.appendChild(newTextFirst);
+list.insertBefore(newItemFirst, list.firstChild);
+
+var listItems = document.querySelectorAll('li');
+
+var i;
+for (i = 0; i< listItems.length; i++) {
+    listItems[i].className = "cool";
+}
+
+var heading = document.querySelector('h2');
+var newItemlast = document.createElement('li');
+var newTextLast = document.createTextNode('cream');
+newItemLast.appendChild(newTextLast);
+list.appendChild(newItemlast);
+    
+var newItemFirst = document.createElement('li');
+var newTextFirst = document.createTextNode('Kale');
+newIteamFirst.appendChild(newTextFirst);
+list.insertBefore(newIteamFirst,firstChild);
+
+var listItems = document.querySelectorAll('li');
+var i;
+for (i = 0; i < listItems.length; i++) {
+    listItems[i].className = 'cool';
+}
+
+var heading = document.querySelector('h2');
+var headingText = heading.firstChild.nodeValue;
+var totalItems = listItems.length;
+var newHeading = headingText + '<span>' + totalItems + '</span>' ;
+heading.innerHTML = newHeading;
